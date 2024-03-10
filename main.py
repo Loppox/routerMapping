a94d56a1494ed1cb5788cbd22afed51bb1ee94eb
Loppox <mettex@gmx.de>
2024-03-10 19:20:06 +0100

    Updated Classes and worked on get_root functiom

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ class Node:
         self.neighbours = {}
         self.name = name
         self.root = root
+        self.root_cost = 0
+        self.message_queue = []
 
-    def get_name(self):
-        return self.name
+    def receive_message(self, content: []):
+        self.message_queue.append(content)
 
-    def add_neighbours(self, neighbour, cost):
-        self.neighbours[neighbour] = cost
+    def broadcast_message(self):
+        costs = list(self.neighbours.values())
+        i = 0
+        message = [self.name, self.root, costs[i]]
+        for neighbour in list(self.neighbours.keys()):
+            neighbour.receive_message(message)
+            i += 1
 
-    def get_neighbours(self):
-        return self.neighbours
+    def update_root(self, root: str, cost: int):
+        self.root = root
+        self.root_cost = cost
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, node: Node):
+        self.nodes.append(node)
 
 
-def parser(nodes_arr):
+def parser(graph: Graph):
     file = open("graph_input", "r")
     lines = file.readlines()
     count = 0
@@ -25,20 +40,39 @@ def parser(nodes_arr):
         elif line.__contains__("}"):
             break
         elif count == 1:
-            nodes_arr.append(Node(line[0], line[4]))
+            graph.nodes.append(Node(line[0], int(line[4])))
         elif count == 2:
             source_dest_cost = line.split(" : ")
             source_dest, cost = source_dest_cost[0], source_dest_cost[1]
             source_node, destination_node = source_dest.split(" - ")
             cost = cost.strip(";")
-            for node in nodes:
+            for node in graph.nodes:
                 if node.name == source_node:
-                    node.add_neighbours(destination_node, int(cost))
+                    for dest_node in graph.nodes:
+                        if dest_node.name == destination_node:
+                            node.neighbours[dest_node] = int(cost)
+    file.close()
 
-def get_root(nodes_arr):
-    print("TLRD: Muss noch gemacht werden")
+
+def get_root(graph: Graph):
+    for node in graph.nodes:
+        if len(node.message_queue) == 0:
+            node.broadcast_message()
+        else:
+            messages = node.message_queue
+            for message in messages:
+                print(message)
+                if node.root > message[1]:
+                    node.update_root(message[1], message[2])
+
+
+
 
 if __name__ == "__main__":
+    graph = Graph()
     nodes = []
-    parser(nodes)
+    parser(graph)
+    get_root(graph)
 
+    for node in graph.nodes:
+        print(node.root)
